7b96b09ea6d7b9c4d08467ce9042e6a1adc0e1a1
Eleanor.Davidson <michaelbdavidson7@gmail.com>
2018-01-03 02:22:21 -0500

    Abstracted personcards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './Star_of_David.svg';
-import placeholderImg from './placeholder.svg';
+// import logo from './Star_of_David.svg';
 import './App.css';
+import PersonCard from './PersonCard';
 
 class App extends Component {
   constructor(props) {
@@ -15,7 +15,7 @@ class App extends Component {
       <div className="App">
         <header className="navbar navbar-expand-lg navbar-dark bg-primary">
           <h1 className="active "> Shul Directory</h1>
-          <img src={logo} className="App-logo my-2 my-lg-0" alt="logo" />
+          {/* <img src={logo} className="App-logo my-2 my-lg-0" alt="logo" /> */}
         </header>
         <br />
         <h3 className="App-intro">
@@ -25,14 +25,7 @@ class App extends Component {
         <div className="col-md-10">
           <div className="card-columns offset-md-3">
             {this.sampleDirectory.map((person) => {
-              return <div className="card" style={{ width: "20rem" }}>
-                <img className="card-img-top" src={placeholderImg} style={{maxheight:286, maxwidth:180}} alt="Member" />
-                <div className="card-body">
-                  <h4 className="card-title">{person.name}</h4>
-                  <a href="#" className="btn btn-secondary">Make bigger</a>
-                </div>
-                <div className="card-footer text-muted">Member since: {person.joinYear}</div>
-              </div>
+              return <PersonCard person={person}  />
             })}
           </div>
         </div>
---
+++ src/PersonCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import placeholderImg from './placeholder.svg';
+
+export default class PersonCard extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        let person = this.props.person;
+       return <div className="card" style={{ width: "20rem" }}>
+            <img className="card-img-top" src={placeholderImg} style={{ maxheight: 286, maxwidth: 180 }} alt="Member" />
+            <div className="card-body">
+                <h4 className="card-title">{person.name}</h4>
+                <a href="#" className="btn btn-secondary">See more</a>
+            </div>
+            <div className="card-footer text-muted">Member since: {person.joinYear}</div>
+        </div>
+    }
+}
