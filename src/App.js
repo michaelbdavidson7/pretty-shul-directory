7ea5ec12257f7c2f493bc30ece2e25950bb0f540
Eleanor.Davidson <michaelbdavidson7@gmail.com>
2018-01-02 22:39:38 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './Star_of_David.svg';
+import placeholderImg from './placeholder.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.sampleDirectory = [{ name: 'Dergus Bergus', joinYear: 2012 }, { name: 'Tergus Nergus', joinYear: 2012 }, { name: 'Plergus Nergus', joinYear: 2012 }, { name: 'Flerg Blergson', joinYear: 2012 }, { name: 'Dergus Bergus', joinYear: 2012 }, { name: 'Tergus Nergus', joinYear: 2012 }, { name: 'Plergus Nergus', joinYear: 2012 }, { name: 'Flerg Blergson', joinYear: 2012 }];
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="navbar navbar-expand-lg navbar-dark bg-primary">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="active">Shul Directory</h1>
+        </header>
+        <br />
+        <h3 className="App-intro">
+          Forget someone's name and it's too late to ask?
+        </h3>
+        <br />
+        <div className="col-md-10">
+          <div className="card-columns offset-md-3">
+            {this.sampleDirectory.map((person) => {
+              return <div className="card" style={{ width: "20rem" }}>
+                <img className="card-img-top" src={placeholderImg} />
+                <div className="card-body">
+                  <h4 className="card-title">{person.name}</h4>
+                  <a href="#" className="btn btn-secondary">Make bigger</a>
+                </div>
+                <div className="card-footer text-muted">Member since: {person.joinYear}</div>
+              </div>
+            })}
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
