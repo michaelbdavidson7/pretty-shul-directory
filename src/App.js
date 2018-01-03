2a6f147861fcaae2ce970dc299672b1df5eec84f
Eleanor.Davidson <michaelbdavidson7@gmail.com>
2018-01-02 23:58:58 -0500

    verbage, colors?

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './Star_of_David.svg';
 import placeholderImg from './placeholder.svg';
 import './App.css';
+import ModalExample from './Modal';
 
 class App extends Component {
   constructor(props) {
@@ -14,19 +15,19 @@ class App extends Component {
     return (
       <div className="App">
         <header className="navbar navbar-expand-lg navbar-dark bg-primary">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="active">Shul Directory</h1>
+          <h1 className="active "> Shul Directory</h1>
+          <img src={logo} className="App-logo my-2 my-lg-0" alt="logo" />
         </header>
         <br />
         <h3 className="App-intro">
           Forget someone's name and now it's too late to ask?
         </h3>
         <br />
-        <div className="col-md-10">
+        <div className="col-md-10 col-sm-12">
           <div className="card-columns offset-md-3">
             {this.sampleDirectory.map((person) => {
               return <div className="card" style={{ width: "20rem" }}>
-                <img className="card-img-top" src={placeholderImg} />
+                <img className="card-img-top" src={placeholderImg} style={{maxheight:286, maxwidth:180}} alt="Member" />
                 <div className="card-body">
                   <h4 className="card-title">{person.name}</h4>
                   <a href="#" className="btn btn-secondary">Make bigger</a>
@@ -36,7 +37,7 @@ class App extends Component {
             })}
           </div>
         </div>
-
+        <ModalExample buttonLabel="asdf" />
       </div>
     );
   }
