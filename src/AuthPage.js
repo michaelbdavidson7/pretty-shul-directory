6bb379cb3f62ae45f91110c895f6c8110a667162
Eleanor.Davidson <michaelbdavidson7@gmail.com>
2018-02-02 13:40:18 -0500

    added link to demo

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -6,6 +6,7 @@ export default class AuthPage extends React.Component {
         return <div>
             <h2>Remember the face, but not the name?</h2>
             <h4>Our free, password-protected picture directory will help you find the name of the person you were looking for</h4>
+            <a href="/demo">Check out the demo</a>
             <br />
             <br />
             <div class="container">
