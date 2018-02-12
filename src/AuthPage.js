272c22c1619787261ae5aff39cf4b23d92381357
Eleanor.Davidson <michaelbdavidson7@gmail.com>
2018-02-11 21:39:55 -0500

    href to home page, text change

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -6,7 +6,7 @@ export default class AuthPage extends React.Component {
     render() {
         return <div>
             <h2>Remember the face, but not the name?</h2>
-            <h4>Our free, password-protected picture directory will help you find the name of the person you were looking for</h4>
+            <h4>Our free, password-protected picture directory will help you find who you were looking for</h4>
             <br />
             {/* <a href="/demo"></a> */}
             <Link to="/demo">Check out the demo</Link>
